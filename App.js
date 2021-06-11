d31015f5634c861631a87d94afa2b00e5622026e
Vadym Mykhaylovych Semchenko <vadym.semchenko@globallogic.com>
2021-06-11 12:10:27 +0300

    add font

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font'
 
 export default function App() {
+
+  const [fontsLoaded, fontsError] = useFonts({
+    'nightfall-bold': require('./assets/black.otf'),
+  })
+  if(fontsError) {
+    console.log('FONTS ERROR', fontsError.message)
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
